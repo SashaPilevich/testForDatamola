00a4fdb1ed253a4b77627ac0616c29e945687aac
Pilevich Sasha <pilevichsasha@gmail.com>
2023-01-10 03:48:51 +0300

    feat: add burger-menu and media query

--- index.js
+++ index.js
@@ -71,3 +71,43 @@ btnPrev.addEventListener('click',() => {
   viewSliders[viewSlide].classList.add('active')
   slider.style.left = -viewSlide * viewport + 'px'
 })
+
+const btnTop = document.getElementById('scrollToTop');
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 500) {
+    btnTop.style.display = 'block';
+  } else {
+    btnTop.style.display = 'none';
+  }
+});
+
+btnTop.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+});
+
+let isOpen = false;
+const burger = document.querySelector('.burger-menu');
+const burgerLine = document.querySelector('.burger-line');
+const mobilenav = document.querySelector('.nav-mobile');
+const link = document.querySelector('.nav-mobile-list');
+
+burger.addEventListener('click', () => {
+  if (!isOpen) {
+    burger.classList.add('active');
+    burgerLine.classList.add('active');
+    mobilenav.classList.add('active');
+    isOpen = true;
+  } else {
+    burger.classList.remove('active');
+    burgerLine.classList.remove('active');
+    mobilenav.classList.remove('active');
+    isOpen = false;
+  }
+});
+
+link.addEventListener("click", () => {
+  burger.classList.remove("active");
+  burgerLine.classList.remove("active");
+  mobilenav.classList.remove("active");
+  isOpen = false;
+});

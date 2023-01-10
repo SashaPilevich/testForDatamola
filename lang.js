2910e848356ceabbfb94f0c5010ec79c0b68c9f9
Pilevich Sasha <pilevichsasha@gmail.com>
2023-01-10 17:39:55 +0300

    refactor:optimize code

--- lang.js
+++ lang.js
@@ -1,62 +1,102 @@
-const langArr = {
-  "nav-link":{
-    "ru":["контакты","образование","английский","проекты","факты обо мне"],
-    "en":["contacts","education", "english", "projects", "facts about me"],
+const langLibrary = {
+  "nav-link": {
+    ru: ["контакты", "образование", "английский", "проекты", "факты обо мне"],
+    en: ["contacts", "education", "english", "projects", "facts about me"],
   },
-  "nav-mobile-link":{
-    "ru":["контакты","образование","английский","проекты","факты обо мне"],
-    "en":["contacts","education", "english", "projects", "facts about me"],
+  "nav-mobile-link": {
+    ru: ["контакты", "образование", "английский", "проекты", "факты обо мне"],
+    en: ["contacts", "education", "english", "projects", "facts about me"],
   },
-  "profile-description":{
-    "ru":"К моему тридцатилетию я стала frontend разработчиком. У меня есть опыт в создании приложений различных уровней сложности. Также я улучшаю и углубляю свои знания (я прохожу обучение в RSSchool, а нахожусь в процессе разработки своего приложения, используя базовые знания Node.js/ Express/ PostgreSQL). Я улучшаю свой английский занятиями с репетитором.",
-    "en":"By the time I was thirty, I had become a frontend developer. I have experience in creating apps of various levels of complexity. I also improve and expand my knowledge (I take training in RSSchool, and I am in the process of developing my application using basic knowledge of Node.js/ Express/ PostgreSQL).I improve my English language skills with tutoring.",
+  "profile-description": {
+    ru: "К моему тридцатилетию я стала frontend разработчиком. У меня есть опыт в создании приложений различных уровней сложности. Также я улучшаю и углубляю свои знания (я прохожу обучение в RSSchool, и нахожусь в процессе разработки своего приложения, используя базовые знания Node.js/ Express/ PostgreSQL). Я улучшаю свой английский занятиями с репетитором.",
+    en: "By the time I was thirty, I had become a frontend developer. I have experience in creating apps of various levels of complexity. I also improve and expand my knowledge (I take training in RSSchool, and I am in the process of developing my application using basic knowledge of Node.js/ Express/ PostgreSQL).I improve my English language skills with tutoring.",
   },
-  "profile-title":{
-    "ru":"Привет, меня зовут",
-    "en":"Hi, My name is",
+  "profile-title": {
+    ru: "Привет, меня зовут Александра Бобрович.",
+    en: "Hi, My name is Aleksandra Bobrovich.",
   },
-  "profile-name":{
-    "ru":"Александра Бобрович",
-    "en":"Aleksandra Bobrovich",
+  "download": {
+    ru: "Загрузить CV",
+    en: "Download CV",
   },
-  "profile-download":{
-    "ru":"Загрузить CV",
-    "en":"download CV",
+  "education-date": {
+    ru: [
+      "Декабрь 2022-сейчас",
+      "Февраль 2022-Ноябрь 2022",
+      "Сентябрь 2011-Июнь 2016",
+    ],
+    en: [
+      "December 2022 - now",
+      "February 2022 - November 2022",
+      "September 2011 - June 2016",
+    ],
   },
-  "education-date":{
-    "ru":["Декабрь 2022-сейчас","Февраль 2022-Ноябрь 2022","Сентябрь 2011-Июнь 2016"],
-    "en":["December 2022 - now","February 2022 - November 2022","September 2011 - June 2016"],
+  "education-specialize": {
+    ru: ["JS/FE Pre-School 2022Q4", "Frontend разработчик", "Провизор"],
+    en: ["JS/FE Pre-School 2022Q4", "Frontend developer", "Pharmacist"],
   },
-  "education-specialize":{
-    "ru":["JS/FE Pre-School 2022Q4","Frontend разработчик","Провизор"],
-    "en":["JS/FE Pre-School 2022Q4","Frontend developer","Pharmacist"],
+  "education-place": {
+    ru: ["Rolling Scopes School", "Teach Me Skills", "ВГМУ"],
+    en: [
+      "Rolling Scopes School",
+      "Teach Me Skills",
+      "Vitebsk state medical university",
+    ],
   },
-  "education-place":{
-    "ru":["Rolling Scopes School","Teach Me Skills","ВГМУ"],
-    "en":["Rolling Scopes School","Teach Me Skills","Vitebsk state medical university"],
+  "project-text": {
+    ru: [
+      "Это приложение позволяет не только найти самые разнообразные кулинарные рецепты, но и мгновенно оформить доставку необходимых продуктов из списка ингредиентов.",
+      "Это приложение позволяет найти замечательные фотографии по различным темам, а также создать свою собственную коллекцию из выбранных фото.",
+      "Лендинг, адаптивный под различные устройства.",
+      "Лендинг, адаптивный под различные устройства.",
+    ],
+    en: [
+      "This is an application that allows you to find not only a wide variety of culinary recipes, but also arrange instant delivery of products from the list of ingredients.",
+      "This application allows you to find beautiful photos on various topics, as well as create your own collections with selected photos.",
+      "This is a simple landing page with adaptive layout for different devices.",
+      "This is a simple landing page with adaptive layout for different devices.",
+    ],
   },
-  "project-text":{
-    "ru":["Это приложение позволяет не только найти самые разнообразные кулинарные рецепты, но и мгновенно оформить доставку необходимых продуктов из списка ингредиентов.","Это приложение позволяет найти замечательные фотографии по различным темам, а также создать свою собственную коллекцию из выбранных фото.","Лендинг, адаптивный под различные устройства.","Лендинг, адаптивный под различные устройства."],
-    "en":["This is an application that allows you to find not only a wide variety of culinary recipes, but also arrange instant delivery of products from the list of ingredients.","This application allows you to find beautiful photos on various topics, as well as create your own collections with selected photos.","This is a simple landing page with adaptive layout for different devices.","This is a simple landing page with adaptive layout for different devices."],
+  "project-view": {
+    ru: ["Проект", "Проект", "Проект", "Проект"],
+    en: ["See project", "See project", "See project", "See project"],
   },
-  "project-view":{
-    "ru":["Проект","Проект","Проект","Проект"],
-    "en":["See project","See project","See project","See project"]
+  "facts-text": {
+    ru: [
+      "кию на Codewars",
+      "раза в неделю занимаюсь английским",
+      "коммитов в 2022",
+      "разработанных приложений в 2022",
+    ],
+    en: [
+      "kyu on Codewars",
+      "times a week study English",
+      "commits in 2022",
+      "applications developed in 2022",
+    ],
   },
-  "facts-text":{
-    "ru":["кию на Codewars","раза в неделю занимаюсь английским","коммитов в 2022","разработанных приложений в 2022"],
-    "en":["kyu on Codewars","times a week study English","commits in 2022","applications developed in 2022"]
+  "footer-date": {
+    ru: "2023 &copy; by Александра Бобрович для DataMola",
+    en: "2023 &copy; by Aleksandra Bobrovich for DataMola",
   },
-  "footer-date":{
-"ru":"2023 &copy; by Александра Бобрович для DataMola",
-"en":"2023 &copy; by Aleksandra Bobrovich for DataMola"
+  "english-text": {
+    ru: "A2 в процессе",
+    en: "A2 in progress",
   },
-  "english-text":{
-    "ru":"A2 в процессе",
-    "en":"A2 in progress"
+  title: {
+    ru: [
+      "Контакты",
+      "Образование и курсы",
+      "Уровень английского",
+      "Проекты",
+      "Факты обо мне",
+    ],
+    en: [
+      "Contacts",
+      "Education and courses",
+      "English",
+      "Projects",
+      "Some facts about me",
+    ],
   },
-  "title":{
-    "ru":["Контакты","Образование и курсы","Уровень английского","Проекты","Факты обо мне"],
-    "en":["Contacts","Education and courses","English","Projects","Some facts about me"]
-  }
-}
+};
